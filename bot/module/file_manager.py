3b76647e1ae210e0bccbfc329795fbd2b91f0d3d
h1ne <henryharlay@gmail.com>
2024-11-15 08:21:50 +0900

    Basic functions

---
+++ bot/module/file_manager.py
@@ -0,0 +1,51 @@
+import os
+from datetime import datetime
+
+class FileInfo:
+    # def __init__(self, name, path, date):
+    def __init__(self, path):
+        # self.filename = filename
+        self.filepath = path
+        # self.date = date
+
+# EN: Check file last modify date
+# JP: ファイルの最終更新日時を取得
+def GetLastModifyDate(filepath):
+    full_filepath = filepath # TODO tmpディレクトリ作成後（パス決定後）に再度設定
+    date_last_modified = os.path.getmtime(full_filepath)
+    mod_time_readable = datetime.fromtimestamp(date_last_modified).strftime('%Y-%m-%d %H:%M:%S') # Format: 2024-07-06 04:19:21
+    return mod_time_readable
+
+# EN: Check file duration (24 hours)
+# JP: ファイルの期限を確認（24時間制限）
+# def File_duration():
+#     return duration
+
+# EN: Check tmp directory size
+# JP: ディレクトリのサイズを取得
+def Directory_size(filepath):
+    full_filepath = filepath # TODO tmpディレクトリ作成後（パス決定後）に再度設定
+
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(full_filepath):
+        for filename in filenames:
+            file_path = os.path.join(dirpath, filename)
+            # シンボリックリンクでないファイルのみサイズを取得
+            if not os.path.islink(file_path):
+                total_size += os.path.getsize(file_path)
+    size_in_megabytes = total_size / (1024 * 1024) # Convert to MB
+
+    return size_in_megabytes
+
+# Delete file
+def Delete(filepath):
+    full_filepath = filepath # TODO tmpディレクトリ作成後（パス決定後）に再度設定
+
+    if os.path.isfile(file_path): # Check if file exists
+        os.remove(file_path)
+        print(f"Deleted {file_path} successfully!")
+    else:
+        print(f"Delete failed. File {file_path} does not exist.")
+
+if __name__ == "__main__":
+	print("main")
