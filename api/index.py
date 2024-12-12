2d5e9af07c1b05196a7d592722d63c98bc091b7e
hisuic <henryharlay@gmail.com>
2024-12-12 23:18:21 +0900

    Add: video download endpoint

--- api/index.py
+++ api/index.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 import logging 
 
 logger = logging.getLogger(__name__)
@@ -11,3 +12,9 @@ app = FastAPI()
 async def root():
     logger.debug("Hello World")
     return [{"hello":"world"}]
+
+# ./tmp_video/afewmomentslater.mp4
+@app.get("/tmp_video/{file_name}")
+async def download_file(file_name: str):
+    file_path = f"tmp_video/{file_name}"
+    return FileResponse(path=file_path, filename=file_name, media_type='application/octet-stream')
