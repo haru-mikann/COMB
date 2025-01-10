385939596b38c5ee361cbbdc1b45b4dfaaae46ea
hisuic <henryharlay@gmail.com>
2025-01-10 12:54:15 +0900

    Add try except

--- bot/module/storageManager.py
+++ bot/module/storageManager.py
@@ -11,9 +11,13 @@ def StorageManager():
     mp4_files = glob.glob(os.path.join(target_dir, "**", "*.mp4"), recursive=True)
     for file_path in mp4_files:
         # print(f"Processing: {file_path}")  # ここに処理を追加
-        ifDelete = file_manager file_manager.isPassed24hours(file_path)
-        if ifDelete == True:
-            file_manager.Delete(file_path)
+        ifDelete = file_manager.isPassed24hours(file_path)
+        try:
+            if ifDelete:
+                print(f"Delete {file_path}") # debug
+                file_manager.Delete(file_path)
+        except ValueError:
+            print(f"Skipping {file_path}: Invalid filename format")
 
 if __name__ == "__main__":
     print("main")
