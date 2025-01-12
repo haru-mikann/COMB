1f24665f74454c4cd6cdbed7342789cc97f4ea78
hisuic <henryharlay@gmail.com>
2025-01-12 14:15:12 +0900

    Fix: used finction to ispassed24hours by path

--- bot/module/storageManager.py
+++ bot/module/storageManager.py
@@ -11,10 +11,10 @@ def StorageManager():
     mp4_files = glob.glob(os.path.join(target_dir, "**", "*.mp4"), recursive=True)
     for file_path in mp4_files:
         # print(f"Processing: {file_path}")  # ここに処理を追加
-        ifDelete = file_manager.isPassed24hours(file_path)
+        ifDelete = file_manager.isPassed24hoursByPath(file_path)
         try:
             if ifDelete:
-                print(f"Delete {file_path}") # debug
+                # print(f"Delete {file_path}") # debug
                 file_manager.Delete(file_path)
         except ValueError:
             print(f"Skipping {file_path}: Invalid filename format")
