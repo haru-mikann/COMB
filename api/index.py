e5283150b7e896088d7cd35f9f2824329288262b
hisuic <henryharlay@gmail.com>
2025-03-08 16:09:37 +0900

    add: video url receiving endpoint: base completed

--- api/index.py
+++ api/index.py
@@ -1,6 +1,11 @@
+import os
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 import logging 
+
+load_dotenv()
+COMMON_PHRASE = os.environ["COMMON_PHRASE"]
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
@@ -12,6 +17,20 @@ app = FastAPI()
 async def root():
     logger.debug("Hello World")
     return [{"hello":"world"}]
+
+@app.get("/vid_url")
+async def get_vid_url(request: Request):
+    auth_header = request.headers.get("Authorization")
+
+    if auth_header != f"Bearer {COMMON_PHRASE}":
+        raise HTTPException(status_code=401, detail="Unauthorized")
+
+    data = await request.json()
+    if "videoURL" not in data:
+        raise HTTPException(status_code=400, detail="Bad Request: 'videoURL' is required")
+    video_url = data["videoURL"]
+
+    Return Responce(status_code = 204)
 
 # ./tmp_video/afewmomentslater.mp4
 @app.get("/tmp_video/{file_name}")
